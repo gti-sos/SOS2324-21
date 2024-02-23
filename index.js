f02b016dd61e906903b65aad8b6e515d5d8593c3
Maria Luisa Rodriguez Cabrera <marrodcab12@alum.us.es>
2024-02-23 19:15:41 +0100

    cambio de ruta contenido index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let app = express();
 
 const PORT = (process.env.PORT || 10000);
 
-app.use("/static/", express.static("./public"));
+app.use(express.static("./public"));
 
 app.get('/cool', (req,res)=> {
     res.send(`<html><body><h1>${cool()}</h1></body></html`);
