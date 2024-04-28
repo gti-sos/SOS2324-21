dd09255380d9dd49e205e60ee163d269623159b5
marrodcab12 <marrodcab12@alum.us.es>
2024-04-28 20:34:30 +0200

    prueba codecov

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,16 @@
 module.exports = {
+    preset: 'jest-playwright-preset',
+    testMatch: ["<rootDir>/tests/jest/**/*.test.cjs"],
+    testEnvironment: "node",
+    setupFilesAfterEnv: ["expect-playwright"],
+    coverageDirectory: "coverage",
+    collectCoverageFrom: [
+        "<rootDir>/front/**/*.svelte",
+        "<rootDir>/front/**/*.js"
+      ],
     collectCoverage: true,
-    coverageReporters: ['text', 'cobertura'],
-}
+    transform: {
+        "^.+\\.svelte$": ["./front/node_modules/svelte-jester/dist/transformer.cjs", { preprocess: true }]
+      },
+  };
+  
