53f40a818a90aba305098b31245439ef66b74fce
jesmenmar <olakasekamikase@gmail.com>
2024-05-16 12:54:09 +0200

    Cambiado el about, subido video JMM y creado carpeta efforts con resumenes toggl

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -1,9 +1,38 @@
 <script>
-    import {dev} from '$app/environment';
-    let mode="production";
-
-    if(dev)
-        mode="development";
+    import { Container, Row, Col } from '@sveltestrap/sveltestrap';
 </script>
-<p></p>
-<p>This is the contact manager app in {mode} mode.</p>
+
+<Container class="column-container">
+    <p></p>
+    <Row>
+        <Col class="text-center">
+            <h1>DEMO Videos</h1>
+        </Col>
+    </Row>
+    <hr/>
+    <Row>
+        <Col sm="6">
+            <h2>Demo de Happiness Reports:</h2>
+            <p></p>
+            <p>Desarrollado por Jesús Manuel Méndez Martínez</p>
+            <p><a href="https://www.youtube.com/watch?v=z0a-BVHuFUo" target="_blank">https://www.youtube.com/watch?v=z0a-BVHuFUo</a></p>
+        </Col>
+        <Col sm="6">
+            <h2>Demo de Cause Of Deaths:</h2>
+            <p></p>
+            <p>Desarrollado por Maria Luisa Rodriguez Cabrera</p>
+            <p><a href="https://sos2324-21.ew.r.appspot.com/about" target="_blank">Enlace</a></p>
+        </Col>
+    </Row>
+
+</Container>
+
+<style>
+    h2 {
+        font-size: 1.5em;
+    }
+
+    h1 {
+        font-size: 2.2rem;
+    }
+</style>
